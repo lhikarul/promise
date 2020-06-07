2dfd2d2a05c84cbde17ed07d3d286de93c1215b9
lhiakrul <711hikaru@gmail.com>
2020-06-07 12:15:38 +0800

    add: after 函數

---
+++ 3.after.js
@@ -0,0 +1,16 @@
+function after(times,callback) {
+    
+    return function() {
+        if (--times === 0) {
+            callback();
+        }
+    }
+}
+
+let fn = after(3,function(){
+    console.log('really');
+})
+
+fn();
+fn();
+fn();
